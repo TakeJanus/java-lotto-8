59c6c0f4df5cc6a09370d8ba32cf4711ab027937
lxxjxxyxxx <dlwnduf7884@naver.com>
2025-11-03 01:01:11 +0900

    feat(Application): main 메서드 및 의존성 주입 구현
    프로그램의 시작점(main) 역할을 하는 Application 클래스를 구현합니다.
    InputView, OutputView, LottoMachine, LottoController 등
    필요한 객체들을 생성하고 의존성을 주입(DI)합니다.
    컨트롤러의 run() 메서드를 호출하여 게임을 시작시킵니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,18 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 의존성 생성
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoMachine lottoMachine = new LottoMachine();
+
+        // 컨트롤러에 의존성 주입 및 실행
+        LottoController lottoController = new LottoController(
+                inputView,
+                outputView,
+                lottoMachine
+        );
+
+        lottoController.run();
     }
 }
